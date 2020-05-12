90b7068644e0c218b099aaf2e1eaeca50f187fab
dariacherbaeva <dashacherbaeva@mail.ru>
2020-05-12 19:10:54 +0300

    registration bug fixed

--- foundation/views.py
+++ foundation/views.py
@@ -19,10 +19,9 @@ def registration_view(request):
             form.save()
             email = form.cleaned_data.get('email')
             raw_password = form.cleaned_data.get('password1')
-            account = authenticate(email=email, password=raw_password)
-            form.save()
-            login(request, account)
-            return redirect('main_page')
+            # account = authenticate(email=email, password=raw_password)
+            # login(request, account)
+            return redirect('login')
         else:
             context['registration_form'] = form
     else:

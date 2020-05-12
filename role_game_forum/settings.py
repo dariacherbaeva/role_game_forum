5e0f6dd6dce954c2582bb61e500e0b5a94759d1f
dariacherbaeva <dashacherbaeva@mail.ru>
2020-05-12 15:33:25 +0300

    settings changed

--- role_game_forum/settings.py
+++ role_game_forum/settings.py
@@ -127,6 +127,7 @@ STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = 'login'
 AUTH_USER_MODEL = 'foundation.SiteUser'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media")
 MEDIA_URL = '/media/'
